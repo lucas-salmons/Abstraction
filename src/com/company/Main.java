38910ff5a9725d7c95053ff373189303567937bd
Lucas Salmons <lucsalmons@gmail.com>
2020-12-31 14:11:58 -0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Dog dog = new Dog("Yorkie");
+	    dog.breathe();
+	    dog.eat();
+
+	    Parrot bird = new Parrot("Australian ringneck");
+	    bird.breathe();
+	    bird.eat();
+		bird.fly();
+
+		Penguin penguin = new Penguin("Emperor");
+		penguin.fly();
+    }
+}
